91a385e554bee8ff5d666e3d1f9dc5056d5ad42c
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-17 14:44:39 +0530

    req.query.n

--- HTTP-SERVER_1/index.js
+++ HTTP-SERVER_1/index.js
@@ -1,15 +1,29 @@
 const express = require("express");
+// const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
-app.get("/shorya-handler", function (req, res) {
-  res.json({
-    name: "Shorya",
-    age: 20,
-  });
-});
+function calculateSum(n) {
+  let ans = 0;
+  for (let index = 1; index <= n; index++) {
+    ans = ans + index;
+  }
+  return ans;
+}
+
+// app.post(bodyParse r.json());
+// app.get("/shorya-handler", function (req, res) {
+//   res.json({
+//     name: "Shorya",
+//     age: 20,
+//   });
+// });
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  const n = req.query.n; //used for adding input to the function that we are calling syntax => localhost:{portnumber}/?n=value
+  const ans = calculateSum(n);
+  // console.log(req.body);
+  res.send("your ans is :" + ans);
 });
 
 app.listen(port, () => {
