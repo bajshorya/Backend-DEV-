e0e060189d18dab293e35498d6590bf58e46bf0d
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-18 16:18:38 +0530

    put and delete functions (kidneys)

--- HTTP-SERVER_1/index2.js
+++ HTTP-SERVER_1/index2.js
@@ -41,4 +41,45 @@ app.post("/", function (req, res) {
   });
 });
 
+app.put("/", function (req, res) {
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    users[0].kidneys[i].healthy = true;
+  }
+  res.json({});
+});
+
+app.delete("/", function (req, res) {
+  if (isThereAtLeastOneUnheathyKidney()) {
+    const newKidneys = [];
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+      if (users[0].kidneys[i].healthy) {
+        newKidneys.push({
+          healthy: true,
+        });
+      }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({ msg: "done!" });
+  } else {
+    res.status(411).json({
+      msg: "you have no bad kidneys",
+    });
+  }
+});
+
+function isThereAtLeastOneUnheathyKidney() {
+  let atLeastOneHealthyKidney = false;
+  for (let i = 0; i < users[0].kidneys.length; i++) {
+    if (!users[0].kidneys[i].healthy) {
+      atLeastOneHealthyKidney = true;
+    }
+  }
+  return atLeastOneHealthyKidney;
+}
+
+app.get("/files/:fileName", function (req, res) {
+  const name = req.params.fileName; //any paramete after localhost:3000/files/ x y z will be redirected here
+  // and we can get that filename using this
+});
+
 app.listen(3000);
