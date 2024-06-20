59e6376d87dec24d7cf2457d0b34f5526c872197
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-20 17:47:15 +0530

    middlewares

--- HTTP-SERVER_1/index3.js
+++ HTTP-SERVER_1/index3.js
@@ -1,21 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-app.get("/health-checkup", (req, res) => {
-  const kidneyId = req.query.kidneyId; // Use 'kidneyid' to match the query parameter
-  const username = req.headers.username;
-  const password = req.headers.password;
-  if (username !== "shorya" && password !== "pass") {
+
+const kidneyId = req.query.kidneyId;
+const username = req.headers.username;
+const password = req.headers.password;
+
+function userMiddleware(req, res, next) {
+  if (username != "shorya" && password != "pass") {
     res.status(403).json({
-      msg: "username/password doesn't exists",
+      msg: "user not found",
     });
-    return;
+  } else {
+    next();
   }
-  if (kidneyId !== "1" && kidneyId !== "2") {
-    res.status(411).json({
-      msg: "wrong inputs",
+}
+
+function kidneyMiddeware(req, res, next) {
+  if (kidneyId != 1 && kidneyId != 2) {
+    res.status(403).json({
+      msg: "Incorrect Inputs",
     });
-    return;
+  } else {
+    next();
   }
+}
+app.use(kidneyMiddeware);
+app.use(userMiddleware);//these will allow us to use  these middlewares withoit mentioning them in the http methods
+// middlewares can only be used adter this line 
+//one more example
+app.use(express.json())
+
+//middlewares are the function which use initial checks before calling the actual functions (usually auth, sign in, inout validations etc.)
+
+
+app.get("/heath-checkup", userMiddleware, kidneyMiddeware, (req, res) => {
+  res.send("Your Heart is healthy !");
 });
+
 app.listen(port);
