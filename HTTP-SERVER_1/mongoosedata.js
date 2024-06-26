4856b3ae4bf3cbc9e6ab0fe2dc9ca4a31cb2f410
SHORYA BAJ <jain.shorya11@gmail.com>
2024-06-26 20:00:17 +0530

    mongoose databse http

--- HTTP-SERVER_1/mongoosedata.js
+++ HTTP-SERVER_1/mongoosedata.js
@@ -1,7 +1,23 @@
+const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
+
+app.use(express.json());
+
 mongoose.connect(
   "mongodb+srv://admin:Y6aH3nOuK2E7y9vc@cluster0.51xfrcy.mongodb.net/"
 );
+
+app.post("/signup", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  const existingUser = await User.findOne({ email: username });
+  if (existingUser) {
+    return res.status(400).send("Username already exists!!!");
+  }
+});
 
 const User = mongoose.model("Users", {
   name: String,
@@ -11,3 +27,4 @@ const User = mongoose.model("Users", {
 
 const user = new User({ name: "b", email: "bcd@123", password: "bcd" });
 user.save();
+app.listen(3000);
