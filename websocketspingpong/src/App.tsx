75aa925a37d54afad58a49335cba9fb72c958f52
SHORYA BAJ <jain.shorya11@gmail.com>
2025-01-06 11:28:58 +0530

    ping pong client server

--- websocketspingpong/src/App.tsx
+++ websocketspingpong/src/App.tsx
@@ -1,15 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [socket, setSocket] = useState();
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   function sendMessage() {
     console.log("Message sent!");
+    const msg = inputRef.current?.value;
+    if (msg !== undefined) {
+      socket?.send(msg);
+    }
   }
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
     ws.onmessage = (message) => {
       alert(message.data);
     };
@@ -17,7 +23,7 @@ function App() {
   return (
     <>
       <div>
-        <input type="text" placeholder="Message...." />
+        <input ref={inputRef} type="text" placeholder="Message...." />
         <button onClick={sendMessage}>Send</button>{" "}
       </div>
     </>
