791f3269e330e2463c450eed8ea4357dd322e8ab
SHORYA BAJ <jain.shorya11@gmail.com>
2025-01-12 23:33:15 +0530

    added multiple socket logic , ausing array and interface

--- chatApp/dist/index.js
+++ chatApp/dist/index.js
@@ -2,12 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
+let allSockets = [];
 let userCount = 0;
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
     userCount += 1;
     console.log("user Connected #" + userCount);
     socket.on("message", (msg) => {
         console.log("message received " + msg.toString());
-        socket.send(msg.toString() + "  msg recvd from sever");
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(msg.toString() + "  msg recvd from sever");
+        }
     });
 });
--- chatApp/src/index.ts
+++ chatApp/src/index.ts
@@ -1,18 +1,30 @@
 import { WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
-let allSockets: WebSocket[] = [];
+
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+let allSockets: User[] = [];
 let userCount = 0;
 wss.on("connection", (socket) => {
-  allSockets.push(socket);
-  userCount += 1;
-  console.log("user Connected #" + userCount);
-
   socket.on("message", (msg) => {
-    console.log("message received " + msg.toString());
-    for (let i = 0; i < allSockets.length; i++) {
-      const s = allSockets[i];
-      s.send(msg.toString() + "  msg recvd from sever");
+    const parsedMessage = JSON.parse(msg.toString()); //because msg will be a string like this "{"type":"send"}"
+    if (parsedMessage.type === "join") {
+      allSockets.push({
+        socket,
+        room: parsedMessage.payload.roomId,
+      });
+    }
+    if (parsedMessage.type == "chat") {
+      //   const currentUserRoom = allSockets.find((x) => x.socket == socket).room;
+      let currentUserRoom = null;
+      for (let i = 0; i < allSockets.length; i++) {
+        if (allSockets[i].socket == socket) {
+          currentUserRoom = allSockets[i].room;
+        }
+      }
     }
   });
 });
